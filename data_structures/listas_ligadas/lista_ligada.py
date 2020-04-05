e93efc9c0d2832ab334f972e95e69b9b48d2b633
MarioGN <mario.geroldineto@gmail.com>
2020-04-05 19:59:19 -0300

    Estruturas de dados: linked list

---
+++ data_structures/listas_ligadas/lista_ligada.py
@@ -0,0 +1,121 @@
+class No():
+    def __init__(self, elemento, proximo=None):
+        self.__elemento = elemento
+        self.__proximo = proximo
+
+    @property
+    def elemento(self):
+        return self.__elemento
+    
+    @elemento.setter
+    def elemento(self, elemento):
+        self.__elemento = elemento
+
+    @property
+    def proximo(self):
+        return self.__proximo
+    
+    @proximo.setter
+    def proximo(self, proximo):
+        self.__proximo = proximo
+
+
+class ListaLigada():
+    def __init__(self):
+        self.__inicio = None
+        self.__fim = None
+        self.__tamanho = 0
+
+    def vazia(self):
+        return self.__tamanho == 0
+
+    def inserir(self, elemento):
+        novo = No(elemento)
+
+        if self.vazia():
+            self.__inicio = novo
+            self.__fim = novo
+        else:
+            self.__fim.proximo = novo
+            self.__fim = novo
+        self.__tamanho += 1
+
+    def recuperar_no(self, posicao):
+        resultado = None
+        if posicao > -1 and posicao < self.__tamanho:
+            for i in range(posicao + 1):
+                if i == 0:
+                    resultado = self.__inicio
+                else:
+                    resultado = resultado.proximo
+
+        return resultado
+
+    def recuperar_elemento(self, posicao):
+        no = self.recuperar_no(posicao)
+        if no:
+            return no.elemento
+        return no
+
+    def inserir_na_posicao(self, posicao, elemento):
+        if posicao > -1 and posicao < self.__tamanho:
+            novo = No(elemento)
+            if posicao == 0:
+                novo.proximo = self.__inicio
+                self.__inicio = novo
+            elif posicao == self.__tamanho:
+                self.__fim.proximo = novo
+                self.__fim = novo
+            else:
+                no_anterior = self.recuperar_no(posicao-1)
+                no_atual = self.recuperar_no(posicao)
+                no_anterior.proximo = novo
+                novo.proximo = no_atual
+            self.__tamanho += 1
+
+    def contem(self, elemento):
+        for i in range(self.__tamanho):
+            no_atual = self.recuperar_no(i)
+            if no_atual.elemento == elemento:
+                return True
+        return False
+
+    def indice(self, elemento):
+        for i in range(self.__tamanho):
+            no_atual = self.recuperar_no(i)
+            if no_atual.elemento == elemento:
+                return i
+        return -1
+
+    def remover_posicao(self, posicao):
+        if posicao > -1 and posicao < self.__tamanho:
+            if posicao == 0:
+                proximo_no = self.__inicio.proximo
+                self.__inicio = None
+                self.__inicio = proximo_no
+            elif posicao == self.__tamanho - 1:
+                penultimo_no = self.recuperar_no(self.__tamanho - 2)
+                penultimo_no.proximo = None
+                self.__fim = penultimo_no
+            else:
+                no_remover = self.recuperar_no(posicao)
+                no_anterior = self.recuperar_no(posicao - 1)
+                no_anterior.proximo = no_remover.proximo
+                no_remover.proximo = None
+            self.__tamanho -= 1
+
+    def remover_elemento(self, elemento):
+        no_remover = self.indice(elemento)
+        if no_remover == -1:
+            print('elemento não existe.')
+        else:
+            self.remover_posicao(no_remover)
+
+    def __str__(self):
+        aux = self.__inicio
+        elementos = ''
+        while aux:
+            elementos = f'{elementos} {aux.elemento}'
+            aux = aux.proximo
+        
+        return elementos
--- data_structures/main.py
+++ data_structures/main.py
@@ -1,31 +1,77 @@
 from vetores import vetor
+from listas_ligadas import lista_ligada
 
 
 if __name__ == "__main__":
-    v = vetor.Vetor()
+    lklist = lista_ligada.ListaLigada()
+    print(lklist.vazia())
 
-    v.inserir_elemento_idx(10, 0)
-    v.inserir_elemento_idx(8, 1)
-    v.inserir_elemento_idx(30, 2)
-    v.inserir_elemento_idx(12, 3)
-    v.inserir_elemento_idx(3, 5)
+    lklist.inserir(1)
+    lklist.inserir(3)
+    lklist.inserir(2)
+    lklist.inserir(5)
+    lklist.inserir(16)
 
-    print(v)
-    print('tamanho: ', v.tamanho())
-    print('elemento 3 ->', v.listar_elemento(3))
+    print(lklist)
+
+    # print(lklist.recuperar_no(-1))
+    # print(lklist.recuperar_no(6))
+    # print(lklist.recuperar_no(3).elemento)
+
+    print(lklist.recuperar_elemento(-1))
+    print(lklist.recuperar_elemento(6))
+    print(lklist.recuperar_elemento(3))
+
+    lklist.inserir_na_posicao(-1, 99)
+    lklist.inserir_na_posicao(100, 20)
+    lklist.inserir_na_posicao(2, 29)
+    lklist.inserir_na_posicao(4, 4)
+    print('\n\n', lklist)
+
+    print(lklist.contem(3))
+    print(lklist.contem(16))
+    print(lklist.contem(233))
+
+    print(lklist.indice(3))
+    print(lklist.indice(16))
+    print(lklist.indice(233))
+
+    print(lklist)
+    lklist.remover_posicao(-1)
+    lklist.remover_posicao(30)
+    lklist.remover_posicao(0)
+    lklist.remover_posicao(5)
+    lklist.remover_posicao(2)
+
+    lklist.remover_elemento(300)
+    lklist.remover_elemento(4)
+    print(lklist)
+
+
+    # v = vetor.Vetor()
+
+    # v.inserir_elemento_idx(10, 0)
+    # v.inserir_elemento_idx(8, 1)
+    # v.inserir_elemento_idx(30, 2)
+    # v.inserir_elemento_idx(12, 3)
+    # v.inserir_elemento_idx(3, 5)
+
+    # print(v)
+    # print('tamanho: ', v.tamanho())
+    # print('elemento 3 ->', v.listar_elemento(3))
     
-    print('contem 20', v.contem(20))
-    print('contem 30 ->', v.contem(30))
+    # print('contem 20', v.contem(20))
+    # print('contem 30 ->', v.contem(30))
 
-    print('idx 20 -> ', v.indice(20))
-    print('idx 30 -> ', v.indice(30))
+    # print('idx 20 -> ', v.indice(20))
+    # print('idx 30 -> ', v.indice(30))
 
-    v.remover_elemento_idx(2)
-    print(v)
-    v.remover_elemento_idx(10)
-    print(v)
-
-    # v.remover_elemento(10)
+    # v.remover_elemento_idx(2)
     # print(v)
-    # v.remover_elemento(99)
+    # v.remover_elemento_idx(10)
     # print(v)
+
+    # # v.remover_elemento(10)
+    # # print(v)
+    # # v.remover_elemento(99)
+    # # print(v)
